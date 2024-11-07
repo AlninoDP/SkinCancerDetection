0e21fb6f1ae93761839bd77fb36a872f7d15c404
Laptop Alnino <alnino007@gmail.com>
2024-11-07 14:28:25 +0700

    wip article

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -58,6 +59,28 @@ dependencies {
 
     // Ucrop
     implementation("com.github.yalantis:ucrop:2.2.8")
+
+    // Retrofit Core, Retrofit Converter Gson, Okhttp Interceptor
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+
+    // Coroutines Core dan Coroutines Android
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
+
+    // Glide
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+
+    // Room Runtime, Room KTX, dan Room Compiler (KSP)
+    implementation("androidx.room:room-runtime:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
+    ksp("androidx.room:room-compiler:2.6.1")
+
+
+    // LiveData dan ViewModel
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
 
 
 
---
+++ app/src/main/java/com/dicoding/asclepius/data/local/entity/NewsEntity.kt
@@ -0,0 +1,4 @@
+package com.dicoding.asclepius.data.local.entity
+
+class NewsEntity {
+}
